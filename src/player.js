1c86413726b7fa3591e8b08da0a2621476ae198b
Howard Engelhart <howard.engelhart@outlook.com>
2013-04-11 14:20:00 -0400

    stopped ended video from going back to beginning

--- src/player.js
+++ src/player.js
@@ -333,11 +333,14 @@ _V_.Player = _V_.Component.extend({
     if (this.options.loop) {
       this.currentTime(0);
       this.play();
-    } else {
+    }
+    /*  HSE -- stop going back to beginning of the video!
+      else {
       this.pause();
       this.currentTime(0);
       this.pause();
     }
+    */
   },
 
   onPlay: function(){
